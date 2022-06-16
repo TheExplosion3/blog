8b513150ab761fa2e3bd0524577be0c5f1a37e33
TheExplosion3 <GOwl2515@gmail.com>
2022-06-15 20:55:48 -0400

    (not a deploy) set up the first blog post page, modified scss stuff, set up 404 page, changed up gitignore a bit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { PageoneComponent } from './pageone/pageone.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     HomepageComponent,
-    AppComponent
+    AppComponent,
+    PageoneComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +23,14 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home-page', component: HomepageComponent},
+      {path: 'post-one', component: PageoneComponent},
+      {path: '', redirectTo: '/home-page', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss']
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  homepageurl = "www.archangelofeuropa.xyz";
+
+  ngOnInit(): void {
+  }
+
+}
